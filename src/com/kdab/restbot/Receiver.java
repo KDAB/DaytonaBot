f2d4e2796e7185b4573f95b74f6581d1762db090
Frank Osterfeld <frank@kdab.com>
2010-08-01 12:59:36 +0200

    tmp

--- src/com/kdab/restbot/Receiver.java
+++ src/com/kdab/restbot/Receiver.java
@@ -2,13 +2,16 @@ package com.kdab.restbot;
 
 import java.util.concurrent.BlockingQueue;
 
-public class Receiver implements Runnable {
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+public class Receiver implements HttpHandler implements Runnable {
 	public Receiver( BlockingQueue<byte[]> outQ ) {
 		m_out = outQ;
 	}
 	
 	public void run() {
-		
+		HttpServer server = new HttpServer;
 	}
 	
 	private BlockingQueue<byte[]> m_out;
