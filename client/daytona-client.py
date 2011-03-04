f154125481d346754b80d261d80a6c0156f761c7
Frank Osterfeld <frank.osterfeld@kdab.com>
2011-03-04 17:03:59 +0100

    add python code snippet for sending message

---
+++ client/daytona-client.py
@@ -0,0 +1,13 @@
+import json
+import urllib2
+
+def sendMessage( map, url ): 
+	msg = json.dumps( map )
+	opener = urllib2.build_opener( urllib2.HTTPHandler )
+	request = urllib2.Request( 'http://localhost:8080/daytona/notify?format=json', data=msg )
+	request.get_method = lambda: 'PUT'
+	url = opener.open( request )
+
+
+if __name__ == "__main__":
+	sendMessage( { 'sender' : 'make-o-matic', 'text' : 'Hello from Python' }, 'http://localhost:8080/daytona/notify?format=json' )
